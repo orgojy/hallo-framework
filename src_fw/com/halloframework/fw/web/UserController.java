5fb332f7974b085f768e0de468aa1029b2aff52a
orgojy <giveawesome@gmail.com>
2022-07-26 01:32:39 +0900

    Add UserController to control requests

---
+++ src_fw/com/halloframework/fw/web/UserController.java
@@ -0,0 +1,35 @@
+package com.halloframework.fw.web;
+
+import com.halloframework.biz.user.User;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserController extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String pathInfo = request.getPathInfo();
+
+        String jspUrl = null;
+
+        if ("/waf/users".contains(pathInfo)) {
+            User user = new User();
+            jspUrl = user.getInfo(request);
+        } else if ("/waf/etc".contains(pathInfo)) {
+            User user = new User();
+            jspUrl = user.getEtc(request);
+        }
+
+        if (jspUrl == null) {
+            throw new ServletException();
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher(jspUrl);
+        dispatcher.forward(request, response);
+    }
+}
