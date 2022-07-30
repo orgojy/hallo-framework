e2d019aa793f62cffbcede91579aeb4506a6451e
orgojy <giveawesome@gmail.com>
2022-07-30 23:57:56 +0900

    Refactor HalloApplication to remove unused CRLF

--- src_fw/com/halloframework/fw/web/HalloApplication.java
+++ src_fw/com/halloframework/fw/web/HalloApplication.java
@@ -88,7 +88,6 @@ public class HalloApplication {
                 socket.close();
             }
         }
-
     }
 
     private static Tomcat tomcat(String tempDir, String webappDir, int port) {
