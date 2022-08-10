4b6de572780d1676f4a6f2d126669ae778d9ab14
orgojy <giveawesome@gmail.com>
2022-08-10 23:33:56 +0900

    Refactor User to inline local variable for method 'getInfo'

--- src_biz/com/halloframework/biz/user/User.java
+++ src_biz/com/halloframework/biz/user/User.java
@@ -5,9 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 public class User {
 
     public String getInfo(HttpServletRequest request) {
-        String nickname = request.getParameter("nickname");
-
-        System.out.println(nickname);
+        System.out.println(request.getParameter("nickname"));
 
         return "/WEB-INF/classes/com/halloframework/biz/user/jsp/list.jsp";
     }
